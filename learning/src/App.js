7ee688bf1ef69dd7c6f90a3809e022c4f2a30fd0
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2025-03-09 13:13:20 +0500

    Add Task

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import MyTodoList from './Components/list';
+import { Fragment } from 'react';
+import PrimaryButton from './Components/button';
 
 function App() {
+
+
+  function greetTheUser() {
+    alert('Hello User ')
+  }
+
+  function contact() {
+    alert('Contact Info ')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <div className="App">
+        Hello World
+      </div>
+
+
+      <MyTodoList />
+
+
+      <PrimaryButton btnFunc={greetTheUser} title="Greet The User" img="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/640px-JavaScript-logo.png" alt="JS-LOGO" />
+      <PrimaryButton btnFunc={contact} title="Second Button" img="https://www.citypng.com/public/uploads/preview/js-javascript-round-logo-icon-png-7017516947717339mnyf7eumy.png" alt="JS-LOGO" />
+      {/* <PrimaryButton title="Second Button" /> */}
+      {/* <PrimaryButton title="Third Button" /> */}
+      {/* <PrimaryButton title="Fourth Button" /> */}
+
+    </Fragment>
+
   );
 }
 
-export default App;
+export default App
